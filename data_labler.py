08ad1886a78ddf00169dc8dfbe8c700c7acf8cb7
xtreemtg <88gigabites@gmail.com>
2019-08-27 08:46:53 -0500

    added arrows but threading is screwy

--- data_labler.py
+++ data_labler.py
@@ -2,31 +2,43 @@ import sys
 
 import chess
 import chess.engine
-from stockfish import Stockfish
+import chess.svg
 
 from FEN_convertor import FEN_convertor
 
 from PyQt5.QtCore import pyqtSlot, Qt
 from PyQt5.QtSvg import QSvgWidget
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QInputDialog, QCheckBox, QMessageBox
+
+import faulthandler
+
+faulthandler.enable()
 
 import threading
-import queue
+import concurrent.futures
 
 sf_file = '/Users/xtreemtg888/Downloads/stockfish-10-mac/Mac/stockfish-10-64'
+komodo_file = '/Users/xtreemtg888/Downloads/komodo-10_ae4bdf/OSX/komodo-10-64-osx'
+engine = chess.engine.SimpleEngine.popen_uci(sf_file)
+engine2 = chess.engine.SimpleEngine.popen_uci(komodo_file)
+STOCKFISH = 'stockfish'
+KOMODO = 'komodo'
+ENGINES = {STOCKFISH: engine, KOMODO: engine2}
+LOCK = threading.Lock()
+
 
 class MainWindow(QWidget):
     """
     Create a surface for the chessboard.
     """
 
-    def __init__(self, board, stockfish):
+    def __init__(self, board):
         """
         Initialize the chessboard.
         """
         super().__init__()
 
-        self.setWindowTitle("Chess GUI")
+        self.setWindowTitle("Chess Cheaters")
         self.setGeometry(300, 300, 800, 800)
 
         self.widgetSvg = QSvgWidget(parent=self)
@@ -41,30 +53,92 @@ class MainWindow(QWidget):
         self.second_click = None
 
         self.board = board
-        self.engine = stockfish
-        self.predict_label = QLabel('test', self)
-        self.predict_label.move(50, 600)
-        self.engine_pred_move()
+        self.predict_label1 = QLabel(STOCKFISH, self)
+        self.predict_label1.move(50, 600)
+        self.predict_label2 = QLabel(KOMODO, self)
+        self.predict_label2.move(50, 650)
+        self.result_label = QLabel('', self)
+        self.result_label.move(650, 195)
+        self.arrows = []
+        button = QPushButton('Takeback', self)
+        button.setToolTip('Take back a move')
+        button.move(650, 75)
+        button.clicked.connect(self.take_back)
+        self.predict_threads()
+        self.C = 0
         self.drawBoard()
 
+    def predict_threads(self):
+        t1 = threading.Thread(target=self.engine_pred_move, args=(STOCKFISH, self.predict_label1))
+        t2 = threading.Thread(target=self.engine_pred_move, args=(KOMODO, self.predict_label2))
+        t1.start()
+        t2.start()
 
-    def engine_pred_move(self):
-        self.predict_label.setText('predicting moves..')
-        self.predict_label.adjustSize()
+    def engine_pred_move(self, engine_name, label):
+        label.setText('predicting moves..')
+        label.adjustSize()
         moves = []
         count = 0
         predict_board = self.board.copy()
         while not predict_board.is_game_over() and count < 10:
-            result = engine.play(predict_board, chess.engine.Limit(time = 0.1))
+            result = ENGINES[engine_name].play(predict_board, chess.engine.Limit(time=0.1))
             move = result.move
             moves.append(move)
             predict_board.push(move)
             count += 1
+        if engine_name == STOCKFISH and len(moves) > 0:
+            self.pred_arrows(moves[0])
         moves = self.board.variation_san(moves)
-        NEXT_MOVES = 'Next few predicted moves:\n'
-        self.predict_label.setText(NEXT_MOVES + moves)
-        self.predict_label.adjustSize()
+        NEXT_MOVES = f'{engine_name} predicted moves:\n'
+        label.setText(NEXT_MOVES + moves)
+        label.adjustSize()
 
+    def pred_arrows(self, move):
+        #TODO Fix the Godamn threading problem that crashes the whole program
+        self.arrows = [(move.from_square, move.to_square)] if move else None
+        self.drawBoard()
+
+
+    def check_promotion(self, coordinates):
+        P = self.pieceToMove[0].symbol()
+        if P == 'P' and self.board.turn:
+            return coordinates[-1] == '8'
+        elif P == 'p' and not self.board.turn:
+            return coordinates[-1] == '1'
+        return False
+
+    def choose_promotion(self):
+        items = {"Queen": 'q', "Rook": 'r', "Bishop": 'b', "Knight": 'n'}
+        item, okPressed = QInputDialog.getItem(self, "Promotion", "Choose the piece to promote to:", items.keys(), 0,
+                                               False)
+        if okPressed and item:
+            return items[item]
+        else:
+            return None
+
+    def check_position(self):
+        if self.board.is_checkmate():
+            self.result_label.setText('checkmate!')
+            self.result_label.adjustSize()
+        elif self.board.is_stalemate():
+            self.result_label.setText('stalemate!')
+            self.result_label.adjustSize()
+        elif self.board.is_insufficient_material():
+            self.result_label.setText('Draw by insufficient material!')
+            self.result_label.adjustSize()
+        else:
+            self.result_label.setText('')
+            self.result_label.adjustSize()
+
+    @pyqtSlot()
+    def take_back(self):
+        try:
+            self.board.pop()
+            self.predict_threads()
+            threading.Thread(target=self.check_position).start()
+            self.drawBoard()
+        except IndexError as e:
+            print('Cannot takeback!')
 
     @pyqtSlot(QWidget)
     def mousePressEvent(self, event):
@@ -84,50 +158,72 @@ class MainWindow(QWidget):
                     piece = self.board.piece_at(square)
                     coordinates = "{}{}".format(chr(file + 97), str(rank + 1))
                     self.second_click = self.pieceToMove[0]
-                    if self.pieceToMove[0] is not None:
-                        move = chess.Move.from_uci("{}{}".format(self.pieceToMove[1], coordinates))
+                    if self.second_click is not None:
+                        if self.check_promotion(coordinates):
+                            new_piece = self.choose_promotion()
+                            if new_piece:
+                                move = chess.Move.from_uci("{}{}{}".format(self.pieceToMove[1], coordinates, new_piece))
+                            else:
+                                return
+                        else:
+                            if self.pieceToMove[1] == coordinates:
+                                print('illegal move!!')
+                                return
+                            move = chess.Move.from_uci("{}{}".format(self.pieceToMove[1], coordinates))
                         if move in self.board.legal_moves:
                             self.board.push(move)
+                            self.predict_threads()
                         piece = None
                         coordinates = None
+                        self.drawBoard()
+                        threading.Thread(target=self.check_position).start()
                     self.pieceToMove = [piece, coordinates]
-                    self.drawBoard()
-                    if self.second_click:
-                        t = threading.Thread(target=self.engine_pred_move)
-                        t.start()
+
+    def render_lock(self, board, lastmove, check):
+        global LOCK
+        LOCK.acquire()
+        try:
+            self.boardSvg = chess.svg.board(board=board, size=self.boardSize, arrows=self.arrows,
+                                            lastmove=lastmove, check=check).encode("UTF-8")
+            self.drawBoardSvg = self.widgetSvg.load(self.boardSvg)
+        finally:
+            LOCK.release()
 
     def drawBoard(self):
         """
         Draw a chessboard with the starting position and then redraw
         it for every new move.
         """
-        self.boardSvg = self.board._repr_svg_().encode("UTF-8")
-        self.drawBoardSvg = self.widgetSvg.load(self.boardSvg)
-
+        board = self.board
+        lastmove = board.peek() if board.move_stack else None
+        check = board.king(board.turn) if board.is_check() else None
+        self.render_lock(board, lastmove, check)
         return self.drawBoardSvg
 
 
 if __name__ == "__main__":
+    print(sys.executable)
     matrix = [
-        ['.', 'K', '.', '.', 'k', '.', '.', '.'],
+        ['.', 'r', '.', 'n', '.', '.', '.', '.'],
+        ['.', '.', '.', 'P', 'k', '.', '.', '.'],
+        ['.', '.', 'P', '.', '.', '.', 'N', 'Q'],
         ['.', '.', '.', '.', '.', '.', '.', '.'],
         ['.', '.', '.', '.', '.', '.', '.', '.'],
         ['.', '.', '.', '.', '.', '.', '.', '.'],
-        ['.', '.', '.', '.', '.', '.', '.', '.'],
-        ['.', '.', '.', '.', '.', '.', '.', '.'],
-        ['.', '.', '.', 'p', '.', '.', '.', '.'],
-        ['.', '.', '.', '.', '.', '.', '.', 'R']
+        ['.', '.', '.', '.', '.', '.', 'p', '.'],
+        ['K', '.', '.', '.', '.', '.', 'N', '.']
 
     ]
 
     cnvrtr = FEN_convertor(matrix)
-    result = cnvrtr.convert('white')
+    result = cnvrtr.convert('black')
     engine = chess.engine.SimpleEngine.popen_uci(sf_file)
+    engine2 = chess.engine.SimpleEngine.popen_uci(komodo_file)
     print(result)
 
     OG = chess.Board(result)
     chessGui = QApplication(sys.argv)
-    window = MainWindow(OG, engine)
+    window = MainWindow(OG)
 
     window.show()
     sys.exit(chessGui.exec_())
